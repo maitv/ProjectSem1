6c2822e08df8763efc2bf8e8547767fea4b7157b
maitv <tranmai.it7@gmail.com>
2014-04-17 19:56:36 +0700

    update database design and add some pages

--- source/js/common.js
+++ source/js/common.js
@@ -23,6 +23,82 @@ function getAccountDisplayResult(tx, results){
 	}
 }
 
+function SetConfigured(db){
+	db.transaction(
+		function(tx){
+		tx.executeSql(
+			"INSERT INTO Config(IsConfig) VALUES(?)",
+			[1],
+			onSuccessExecuteSql,
+			onErrorCommon
+		)
+		},
+		onErrorCommon,
+		onReadyTransaction
+	);
+}
+	
+function createTableConfig(db){
+	db.transaction(
+		function(tx){
+		tx.executeSql(
+			"CREATE TABLE IF NOT EXISTS Config (IsConfig BOOL)",
+			[],
+			onSuccessExecuteSql,
+			onErrorCommon
+		)
+		},
+		onErrorCommon,
+		onReadyTransaction
+	);
+}
+
+
+function createTableAccountInfo(db){
+	db.transaction(
+		function(tx){
+			tx.executeSql(
+				"CREATE TABLE IF NOT EXISTS AccountInfo (AccountID INTEGER PRIMARY KEY AUTOINCREMENT, AccountDisplay TEXT, AccountBalance INTEGER, CurrencyUnit TEXT)",
+				[],
+				onSuccessExecuteSql,
+				onErrorCommon
+			)
+		},
+		onErrorCommon,
+		onReadyTransaction
+	);
+}
+
+function createTableCategory(db){
+	db.transaction(
+		function(tx){
+			tx.executeSql(
+				"CREATE TABLE IF NOT EXISTS Category (CategoryID INTEGER PRIMARY KEY AUTOINCREMENT, CategoryName TEXT, CategoryType INTEGER, CategoryTypeSpecify INTEGER)",
+				[],
+				onSuccessExecuteSql,
+				onErrorCommon
+			)
+		},
+		onErrorCommon,
+		onReadyTransaction
+	);
+}
+
+function createTableTransaction(db){
+	db.transaction(
+		function(tx){	
+			tx.executeSql(
+				"CREATE TABLE IF NOT EXISTS Transactions (TransactionID INTEGER PRIMARY KEY AUTOINCREMENT, Amount INTEGER, Description TEXT, CategoryID INTEGER, Note TEXT, TransactionDate DATE)",
+				[],
+				onSuccessExecuteSql,
+				onErrorCommon
+			)
+		},
+		onErrorCommon,
+		onReadyTransaction
+	);
+}
+
 function getAccountDisplay(db){
 	db.transaction(
 					function(tx){				
