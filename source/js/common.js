05dcdb82359c93cccc0b4039aab5a1b645659838
maitv <tranmai.it7@gmail.com>
2014-05-05 07:22:55 +0700

    update trends

--- source/js/common.js
+++ source/js/common.js
@@ -726,3 +726,31 @@ function onNextMonthDebt(){
 	
 	updateMonthDisplayForDebt();
 }
+
+function drawPieChart(){
+	google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+        chart.draw(data, options);
+      }
+
+}
+
+function onTrends(){
+	resetFields();
+	drawPieChart();	
+}
